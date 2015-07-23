0bb22c4ed3b2441d9827f0f991428635f820f56d
Dmitrii Krasheninnikov <dmkr0001@gmail.com>
2015-07-23 18:38:33 +0300

    put make_window_list function into islands module

--- broadPeaks.py
+++ broadPeaks.py
@@ -11,7 +11,6 @@ import scipy.stats
 import argparse
 import arguments
 import pre_counting
-import windows
 import islands
 import output
 
@@ -95,7 +94,7 @@ l0 = scipy.stats.poisson.ppf(1 - p0, lambdaa)
 print(l0)
 
 print("Finished counting reads, now making window list")
-windowList = windows.make_windows_list(bamPath, chromosomes_info, l0, WINDOW_SIZE, GAP, total_unique_reads_count)
+windowList = islands.make_windows_list(bamPath, chromosomes_info, l0, WINDOW_SIZE, GAP, total_unique_reads_count)
 print("Finished window list, now making island list")
 island_list = islands.make_islands_list(windowList, lambdaa, WINDOW_SIZE, l0, chromosomes_info, ISLAND_SCORE_THRESHOLD)
 output.write_output(outfile, island_list)
